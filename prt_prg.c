b5f471676d3ad013db39bc79a10d8b8b5a8bacc9
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-17 13:13:29 +0300

    commit

--- prt_prg.c
+++ prt_prg.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
-* print_prg - writes the character c to stdout
+* prt_prg - writes the character c to stdout
 * @a: input char
 * @buff: buffer pointer
 * @i: index for buffer pointer
 * Return: On success 1.
 */
 
-int print_prg(va_list a __attribute__((unused)), char *buff, unsigned int i)
+int prt_prg(va_list a __attribute__((unused)), char *buff, unsigned int i)
 {
 handl_buff(buff, '%', i);
 return (1);
