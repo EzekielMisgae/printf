87683f9d2402304f19b273ca661bd10cb5d43944
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-17 13:11:41 +0300

    commit

--- prt_oct.c
+++ prt_oct.c
@@ -1,46 +1,45 @@
-
-   
 #include "main.h"
 
 /**
- * print_oct - prints decimal number in octal
- * @arguments: input number
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed.
- */
-int print_oct(va_list arguments, char *buf, unsigned int ibuf)
-{
-	int int_input, i, isnegative, count, first_digit;
-	char *octal, *binary;
+* prt_oct - prints decimal number in octal
+* @arguments: input number
+* @buff: buffer pointer
+* @ibuff: index for buffer pointer
+* Return: number of chars printed.
+*/
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
-	if (int_input == 0)
-	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	octal = malloc(sizeof(char) * (11 + 1));
-	octal = fill_oct_array(binary, octal);
-	for (first_digit = i = count = 0; octal[i]; i++)
-	{
-		if (octal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, octal[i], ibuf);
-			count++;
-		}
-	}
-	free(binary);
-	free(octal);
-	return (count);
+int prt_oct(va_list arguments, char *buff, unsigned int ibuff)
+{
+int int_input, i, isnegative, count, first_digit;
+char *octal, *binary;
+
+int_input = va_arg(arguments, int);
+isnegative = 0;
+if (int_input == 0)
+{
+ibuff = handl_buff(buff, '0', ibuff);
+return (1);
+}
+if (int_input < 0)
+{
+int_input = (int_input * -1) - 1;
+isnegative = 1;
+}
+binary = malloc(sizeof(char) * (32 + 1));
+binary = fill_binary_array(binary, int_input, isnegative, 32);
+octal = malloc(sizeof(char) * (11 + 1));
+octal = fill_oct_array(binary, octal);
+for (first_digit = i = count = 0; octal[i]; i++)
+{
+if (octal[i] != '0' && first_digit == 0)
+first_digit = 1;
+if (first_digit)
+{
+ibuff = handl_buff(buff, octal[i], ibuff);
+count++;
+}
+}
+free(binary);
+free(octal);
+return (count);
 }
