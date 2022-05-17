f724f29b40ead8187ae5c8346bfc4e1ea1e7cbb1
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-17 13:15:10 +0300

    commit

--- prt_rev.c
+++ prt_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* print_rev - writes the str in reverse
+* prt_rev - writes the str in reverse
 * @arguments: input string
-* @buf: buffer pointer
-* @ibuf: index for buffer pointer
+* @buff: buffer pointer
+* @ibuff: index for buffer pointer
 * Return: number of chars printed.
 */
 
-int print_rev(va_list arguments, char *buf, unsigned int ibuf)
+int prt_rev(va_list arguments, char *buff, unsigned int ibuff)
 {
 char *str;
 unsigned int i;
@@ -19,7 +19,7 @@ str = va_arg(arguments, char *);
 if (str == NULL)
 {
 for (i = 0; nill[i]; i++)
-ibuf = handl_buf(buf, nill[i], ibuf);
+ibuff = handl_buff(buff, nill[i], ibuff);
 return (6);
 }
 for (i = 0; str[i]; i++)
@@ -27,7 +27,7 @@ for (i = 0; str[i]; i++)
 j = i - 1;
 for (; j >= 0; j--)
 {
-ibuf = handl_buf(buf, str[j], ibuf);
+ibuff = handl_buff(buff, str[j], ibuff);
 }
 return (i);
 }
