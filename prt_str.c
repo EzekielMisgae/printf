b7df0d96791725eb4e7b71428e3085f974a0ae41
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-17 13:19:50 +0300

    commit

--- prt_str.c
+++ prt_str.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* print_str - writes the string to stdout
+* prt_str - writes the string to stdout
 * @arguments: input string
-* @buf: buffer pointer
-* @ibuf: index for buffer pointer
+* @buff: buffer pointer
+* @ibuff: index for buffer pointer
 * Return: On success 1.
 */
 
-int print_str(va_list arguments, char *buf, unsigned int ibuf)
+int prt_str(va_list arguments, char *buff, unsigned int ibuff)
 {
 char *str;
 unsigned int i;
@@ -18,10 +18,10 @@ str = va_arg(arguments, char *);
 if (str == NULL)
 {
 for (i = 0; nill[i]; i++)
-ibuf = handl_buf(buf, nill[i], ibuf);
+ibuff = handl_buff(buff, nill[i], ibuff);
 return (6);
 }
 for (i = 0; str[i]; i++)
-ibuf = handl_buf(buf, str[i], ibuf);
+ibuff = handl_buff(buff, str[i], ibuff);
 return (i);
 }
