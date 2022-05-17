be4a538d294b871b128d0773ebddd4680c1fd23e
EzekielMisgae <ezkielmisgae@gmail.com>
2022-05-17 12:51:20 +0300

    commit

--- print_num_hex.c
+++ print_num_hex.c
@@ -1,46 +1,47 @@
 #include "main.h"
 
 /**
- * prinnhex - print number in hex begining with zero
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
- */
-int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
-{
-	int int_input, i, isnegative, count, first_digit;
-	char *hexadecimal, *binary;
+* pnhex - print number in hex begining with zero
+* @arguments: input string
+* @buff: buffer pointer
+* @ibuff: index for buffer pointer
+* Return: number of chars printed
+*/
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
-	if (int_input == 0)
-	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
-	ibuf = handl_buf(buf, '0', ibuf);
-	ibuf = handl_buf(buf, 'x', ibuf);
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	hexadecimal = malloc(sizeof(char) * (8 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
-	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-			count++;
-		}
-	}
-	free(binary);
-	free(hexadecimal);
-	return (count + 2);
+int pnhex(va_list arguments, char *buff, unsigned int ibuff)
+{
+int int_input, i, isnegative, count, first_digit;
+char *hexadecimal, *binary;
+
+int_input = va_arg(arguments, int);
+isnegative = 0;
+if (int_input == 0)
+{
+ibuff = handl_buff(buff, '0', ibuff);
+return (1);
+}
+if (int_input < 0)
+{
+int_input = (int_input * -1) - 1;
+isnegative = 1;
+}
+ibuff = handl_buff(buff, '0', ibuff);
+ibuff = handl_buff(buff, 'x', ibuff);
+binary = malloc(sizeof(char) * (32 + 1));
+binary = fill_binary_array(binary, int_input, isnegative, 32);
+hexadecimal = malloc(sizeof(char) * (8 + 1));
+hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
+for (first_digit = i = count = 0; hexadecimal[i]; i++)
+{
+if (hexadecimal[i] != '0' && first_digit == 0)
+first_digit = 1;
+if (first_digit)
+{
+ibuff = handl_buff(buff, hexadecimal[i], ibuff);
+count++;
+}
+}
+free(binary);
+free(hexadecimal);
+return (count + 2);
 }
