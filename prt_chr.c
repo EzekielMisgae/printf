c4cb0163128632a5af23267f69ad641a972cd3ad
EzekielMisgae <101397453+EzekielMisgae@users.noreply.github.com>
2022-05-17 10:39:04 +0300

    commit

--- prt_chr.c
+++ prt_chr.c
@@ -1,1 +1,20 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+* prt_chr - writes the character c to stdout
+* @arguments: input char
+* @buff: buffer pointer
+* @ibuf: index for buffer pointer
+* Return: On success 1.
+*/
+
+int prt_chr(va_list arguments, char *buff, unsigned int ibuff)
+{
+char c;
+
+c = va_arg(arguments, int);
+handl_buff(buff, c, ibuff);
+
+return (1);
+}
